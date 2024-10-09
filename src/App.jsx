5b63285ad6feb588cbfa28a97449069834ddef7b
Sunil Surendran <133184647+SunilSurendran1906@users.noreply.github.com>
2024-10-09 13:07:09 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import Player from "./components/Player";
+import TimerChallenge from "./components/TimerChallenge";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1} />
+        <TimerChallenge title="Not easy" targetTime={5} />
+        <TimerChallenge title="Getting tough" targetTime={10} />
+        <TimerChallenge title="Pros only" targetTime={15} />
+        {/* <TimerChallenge title="Hard level" targetTime={20} />
+        <TimerChallenge title="Experts only" targetTime={25} /> */}
+      </div>
     </>
   );
 }
